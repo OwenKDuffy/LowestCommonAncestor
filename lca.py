4a7fc684bc23e0ceb3a414b83aebb01fcf7499ee
Owen Duffy <duffyow@tcd.ie>
2018-10-01 10:07:31 +0100

    First Code

---
+++ lca.py
@@ -0,0 +1,8 @@
+class Node:
+    # Constructor to create a new binary node
+    def __init__(self, val):
+        self.val =  val
+        self.left = None
+        self.right = None
+
+def lowestCommonAncestor(root, x, y)
