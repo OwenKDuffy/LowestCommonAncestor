f0614221133b072b719de64db59a2a645641f388
Owen Duffy <duffyow@tcd.ie>
2018-10-02 13:58:14 +0100

    added comments removed redundant code and added source of original

--- lca.py
+++ lca.py
@@ -6,7 +6,7 @@ class Node:
         self.right = None
 
 def lowestCommonAncestor(root, x, y):
-    
+
     #create paths
     path1 = []
     path2 = []
@@ -15,37 +15,39 @@ def lowestCommonAncestor(root, x, y):
         return -1
 
 
-    i = 0 
-    while(i < len(path1) and i < len(path2)): 
-        if path1[i] != path2[i]: 
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
             break
         i += 1
-    return path1[i-1] 
-  
+    return path1[i-1]
 
-def findPath( root, path, k): 
-  
-    # Baes Case 
-    if root is None: 
+
+def findPath( root, path, k):
+
+    # Baes Case
+    if root is None:
         return False
-  
-    # Store this node is path vector. The node will be 
-    # removed if not in path from root to k 
-    path.append(root.val) 
-  
-    # See if the k is same as root's val 
-    if root.val == k : 
-        return True
-  
-    # Check if k is found in left or right sub-tree 
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))): 
-        return True 
-  
-    # If not present in subtree rooted with root, remove 
-    # root from path and return False 
-       
-    path.pop() 
-    return False
-  
 
+    # Store this node is path vector. The node will be
+    # removed if not in path from root to k
+    path.append(root.val)
+
+    # See if the k is same as root's val
+    if root.val == k :
+        return True
+
+    # Check if k is found in left or right sub-tree
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+
+    # If not present in subtree rooted with root, remove
+    # root from path and return False
+
+    path.pop()
+    return False
+
+
+# This code is modified from code by Nikhil Kumar Singh(nickzuck_007) at
+#https://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
--- test_lca.py
+++ test_lca.py
@@ -4,6 +4,7 @@ import lca
 
 class test_lca(unittest.TestCase):
 
+    #test a simple tree/base case
     def test_basicTree(self):
         root = lca.Node(1)
         root.left = lca.Node(2)
@@ -14,10 +15,12 @@ class test_lca(unittest.TestCase):
         root.right.right = lca.Node(7)
         self.assertEqual(2, lca.lowestCommonAncestor(root, 4, 5))
 
+    #test on an empty tree expect error
     def test_nullTree(self):
         root = None
         self.assertEqual(-1, lca.lowestCommonAncestor(root, 4, 5), 'Empty tree returns -1')
 
+    #test for a node which is not contained in the tree
     def test_InvalidNode(self):
         root = lca.Node(1)
         root.left = lca.Node(2)
@@ -28,6 +31,7 @@ class test_lca(unittest.TestCase):
         root.right.right = lca.Node(7)
         self.assertEqual(-1, lca.lowestCommonAncestor(root, 4, 8), "Unfound node returns -1")
 
+    #test for case when the common ancestor is one of the query nodes
     def test_commonAncestorIsNode(self):
         root = lca.Node(1)
         root.left = lca.Node(2)
@@ -40,22 +44,8 @@ class test_lca(unittest.TestCase):
         self.assertEqual(2, lca.lowestCommonAncestor(root, 2, 2), "Common Ancestor of 2 & 2 is 2 itself")
         self.assertEqual(2, lca.lowestCommonAncestor(root, 4, 2), "Common Ancestor of 4 & 2 is 2 itself")
 
-
+        ## TODO: add more edge case tests as i think of exceptional circuimstances
 
 
 if __name__ == '__main__':
     unittest.main()
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram
-#root = lca.Node(1)
-#root.left = lca.Node(2)
-#root.right = lca.Node(3)
-#root.left.left = lca.Node(4)
-#root.left.right = lca.Node(5)
-#root.right.left = lca.Node(6)
-#root.right.right = lca.Node(7)
-
-#print ("LCA(4, 5) = %d" %(lowestCommonAncestor(root, 4, 5)))
-#print ("LCA(4, 6) = %d" %(lowestCommonAncestor(root, 4, 6)))
-#print ("LCA(3, 4) = %d" %(lowestCommonAncestor(root,3,4)))
-#print ("LCA(2, 4) = %d" %(lowestCommonAncestor(root,2, 4)))
