32b869960fbe284471ce97eb4dee5aeeb2b0fde5
Owen Duffy <duffyow@tcd.ie>
2018-10-02 13:49:05 +0100

    This branch to redesign the code to use a different recursive method

--- lca.py
+++ lca.py
@@ -6,46 +6,53 @@ class Node:
         self.right = None
 
 def lowestCommonAncestor(root, x, y):
-    
+
     #create paths
-    path1 = []
-    path2 = []
+    path = []
 
-    if (not findPath(root, path1, x) or not findPath(root, path2, y)):
-        return -1
+    find(root, x, y, path)
+    # if (not findPath(root, path1, x) or not findPath(root, path2, y)):
+    #     return -1
+    #
+    #
+    # i = 0
+    # while(i < len(path1) and i < len(path2)):
+    #     if path1[i] != path2[i]:
+    #         break
+    #     i += 1
+    # return path1[i-1]
+    #
+def find(candidate, x, y, path):
+    if(candidate.val == x)
+        #do thing
+        findSecondVal(path)
+    else if(candidate.val == y)
+        #do other thing
+    else
+        find(candidate.left, x, y, path)
 
 
-    i = 0 
-    while(i < len(path1) and i < len(path2)): 
-        if path1[i] != path2[i]: 
-            break
-        i += 1
-    return path1[i-1] 
-  
+def findPath( root, path, k):
 
-def findPath( root, path, k): 
-  
-    # Baes Case 
-    if root is None: 
+    # Baes Case
+    if root is None:
         return False
-  
-    # Store this node is path vector. The node will be 
-    # removed if not in path from root to k 
-    path.append(root.val) 
-  
-    # See if the k is same as root's val 
-    if root.val == k : 
-        return True
-  
-    # Check if k is found in left or right sub-tree 
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))): 
-        return True 
-  
-    # If not present in subtree rooted with root, remove 
-    # root from path and return False 
-       
-    path.pop() 
-    return False
-  
 
+    # Store this node is path vector. The node will be
+    # removed if not in path from root to k
+    path.append(root.val)
+
+    # See if the k is same as root's val
+    if root.val == k :
+        return True
+
+    # Check if k is found in left or right sub-tree
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+
+    # If not present in subtree rooted with root, remove
+    # root from path and return False
+
+    path.pop()
+    return False
--- test_lca.py
+++ test_lca.py
@@ -45,17 +45,3 @@ class test_lca(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram
-#root = lca.Node(1)
-#root.left = lca.Node(2)
-#root.right = lca.Node(3)
-#root.left.left = lca.Node(4)
-#root.left.right = lca.Node(5)
-#root.right.left = lca.Node(6)
-#root.right.right = lca.Node(7)
-
-#print ("LCA(4, 5) = %d" %(lowestCommonAncestor(root, 4, 5)))
-#print ("LCA(4, 6) = %d" %(lowestCommonAncestor(root, 4, 6)))
-#print ("LCA(3, 4) = %d" %(lowestCommonAncestor(root,3,4)))
-#print ("LCA(2, 4) = %d" %(lowestCommonAncestor(root,2, 4)))
