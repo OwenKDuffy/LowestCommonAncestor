4bf37279dfb67ec9ed5c084b40fcd8cd3ec91e5b
Owen <duffyow@tcd.ie>
2018-10-25 23:28:28 +0100

    ADG now working

--- lca.py
+++ lca.py
@@ -2,58 +2,74 @@ class Node:
     # Constructor to create a new binary node
     def __init__(self, val):
         self.val =  val
-        # self.children[0] = None
-        # self.children[1] = None
         self.children = []
-        #graph is no longer binary tree so could have more than two children
+        self.ancestors = []
+        self.maxDepth = 0;
+                                                    #graph is no longer binary tree so could have more than two children
+
+    def addChild(self, n):
+        self.children.append(n)                     #give this node new child n
+        n.ancestors.extend(self.ancestors)          #give n all of this nodes ancestors
+        n.ancestors.append(self)                    #add this node as an ancestor of n's
+        if n.maxDepth < self.maxDepth + 1:
+            n.maxDepth = self.maxDepth + 1          #if this Node is the deepest ancestor of n make n one deeper
+
+    def printAncestors(self):
+        print ("Ancestors of", self.val)
+        for x in self.ancestors:
+            print ("Parent = " , x.val , ", Depth = " , x.maxDepth , "\n")
+
 
 def lowestCommonAncestor(root, x, y):
-
-    #create paths
-    path1 = []
-    path2 = []
-
-    if (not findPath(root, path1, x) or not findPath(root, path2, y)):
+    if root == None:                                 #there is no tree return error
         return -1
 
 
-    i = 0
-    while(i < len(path1) and i < len(path2)):
-        if path1[i] != path2[i]:
-            break
-        i += 1
-    return path1[i-1]
+    # find two nodes
+    n1 = findNode(root, x)
+    n2 = findNode(root, y)
 
 
-def findPath( root, path, k):
-
-    # Baes Case
-    if root is None:
-        return False
-
-    # Store this node is path vector. The node will be
-    # removed if not in path from root to k
-    path.append(root.val)
-
-    # See if the k is same as root's val
-    if root.val == k :
-        return True
-
-    # check if there are subtrees
-    if (len(root.children)>=1):
-        pass
-        # Check if k is found in left or right sub-tree
-        if ((root.children[0] != None and findPath(root.children[0], path, k))):
-            return True
-        elseif(( len(root.children) >= 2) and root.children[1]!= None and findPath(root.children[1], path, k)):
-            return True
-
-    # If not present in subtree rooted with root, remove
-    # root from path and return False
-
-    path.pop()
-    return False
+    if n1 == n2:
+        return n1.val                               #LCA of same node is itself
 
 
-# This code is modified from code by Nikhil Kumar Singh(nickzuck_007) at
-#https://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+    if n1 == None or n2 == None:                    #error no such node exists
+        return -1
+
+
+    deepestAncestorDepth = -1
+    deepestAncestor = None
+    for i in n1.ancestors:
+        if i in n2.ancestors:
+            if i.maxDepth > deepestAncestorDepth:
+                deepestAncestor = i
+                deepestAncestorDepth = i.maxDepth
+
+    #check if node itself is not an ancestor
+    if n1 in n2.ancestors:                          #if n1 is an ancestor of n2
+        if n1.maxDepth > deepestAncestorDepth:      #and if n1 is deeper than previous deepest ancestor
+            deepestAncestor = n1
+            deepestAncestorDepth = n1.maxDepth
+
+    #vice versa
+    if n2 in n1.ancestors:                         #if n2 is an ancestor of n1
+        if n2.maxDepth > deepestAncestorDepth:     #and if n2 is deeper than previous deepest ancestor
+            deepestAncestor = n2
+            deepestAncestorDepth = n2.maxDepth
+
+
+
+    return deepestAncestor.val
+
+
+def findNode(node, val):
+    if node.val == val:
+        return node
+    elif len(node.children) != 0:
+        for x in node.children:
+            n = findNode(x, val)
+            if n != None:
+                return n
+    else:
+        return None
--- test_lca.py
+++ test_lca.py
@@ -1,19 +1,71 @@
 import unittest
 import lca
 
+def createBinaryTreeStyleGraph():
+    root = lca.Node(1)
+    n2 = lca.Node(2)
+    n3 = lca.Node(3)
+    n4 = lca.Node(4)
+    n5 = lca.Node(5)
+    n6 = lca.Node(6)
+    n7 = lca.Node(7)
+    root.addChild(n2)
+    root.addChild(n3)
+    n2.addChild(n4)
+    n2.addChild(n5)
+    n3.addChild(n6)
+    n3.addChild(n7)
+    return root
+
+
+def createADG():
+    root = lca.Node(1)
+    n2 = lca.Node(2)
+    n3 = lca.Node(3)
+    n4 = lca.Node(4)
+    n5 = lca.Node(5)
+    n6 = lca.Node(6)
+    n7 = lca.Node(7)
+    n8 = lca.Node(8)
+    n9 = lca.Node(9)
+    n10 = lca.Node(10)
+    n11 = lca.Node(11)
+    n12 = lca.Node(12)
+    n13 = lca.Node(13)
+    n14 = lca.Node(14)
+    root.addChild(n2)
+    root.addChild(n3)
+    n2.addChild(n5)
+    n2.addChild(n4)
+    n3.addChild(n4)
+    n3.addChild(n7)
+    n4.addChild(n11)
+    n4.addChild(n6)
+    n5.addChild(n8)
+    n6.addChild(n10)
+    n6.addChild(n13)
+    n7.addChild(n9)
+    n8.addChild(n10)
+    n9.addChild(n12)
+    n10.addChild(n14)
+    n11.addChild(n10)
+    n13.addChild(n12)
+    n13.addChild(n14)
+    return root
 
 class test_lca(unittest.TestCase):
 
-    #test a simple tree/base case
+    #Housekeeping, these functions make the tests neater to look at
+
+
+
+    # test a simple binary tree/base case
     def test_basicTree(self):
-        root = lca.Node(1)
-        root.children.append(lca.Node(2))
-        root.children.append(lca.Node(3))
-        root.children[0].children.append(lca.Node(4))
-        root.children[0].children.append(lca.Node(5))
-        root.children[1].children.append(lca.Node(6))
-        root.children[0].children.append(lca.Node(7))
+        root = createBinaryTreeStyleGraph()
         self.assertEqual(2, lca.lowestCommonAncestor(root, 4, 5))
+        self.assertEqual(1, lca.lowestCommonAncestor(root, 4, 6))
+
+
 
     #test on an empty tree expect error
     def test_nullTree(self):
@@ -22,29 +74,38 @@ class test_lca(unittest.TestCase):
 
     #test for a node which is not contained in the tree
     def test_InvalidNode(self):
-        root = lca.Node(1)
-        root.children.append(lca.Node(2))
-        root.children.append(lca.Node(3))
-        root.children[0].children.append(lca.Node(4))
-        root.children[0].children.append(lca.Node(5))
-        root.children[1].children.append(lca.Node(6))
-        root.children[0].children.append(lca.Node(7))
+        root = createBinaryTreeStyleGraph()
         self.assertEqual(-1, lca.lowestCommonAncestor(root, 4, 8), "Unfound node returns -1")
 
     #test for case when the common ancestor is one of the query nodes
     def test_commonAncestorIsNode(self):
-        root = lca.Node(1)
-        root.children.append(lca.Node(2))
-        root.children.append(lca.Node(3))
-        root.children[0].children.append(lca.Node(4))
-        root.children[0].children.append(lca.Node(5))
-        root.children[1].children.append(lca.Node(6))
-        root.children[0].children.append(lca.Node(7))
-        self.assertEqual(2, lca.lowestCommonAncestor(root, 2, 4), "Common Ancestor of 2 & 4 is 2 itself")
+        root = createBinaryTreeStyleGraph()
+        self.assertEqual(2, lca.lowestCommonAncestor(root, 2, 4), "Common Ancestor of 2 & 4 is 2")
         self.assertEqual(2, lca.lowestCommonAncestor(root, 2, 2), "Common Ancestor of 2 & 2 is 2 itself")
         self.assertEqual(2, lca.lowestCommonAncestor(root, 4, 2), "Common Ancestor of 4 & 2 is 2 itself")
 
+    def test_simpleTree(self):
+        n4 = lca.Node(4)
+        n11 = lca.Node(11)
+        n6 = lca.Node(6)
+        n4.addChild(n11)
+        n4.addChild(n6)
+        self.assertEqual(4, lca.lowestCommonAncestor(n4, 11, 6), "Common Ancestor of 11 & 6 is 4")
+
+
+    def test_multipleRoutes(self):
+        root = createADG()
+        # lca.findNode(root, 10).printAncestors() these were for debugging
+        # lca.findNode(root, 12).printAncestors()
+        self.assertEqual(6, lca.lowestCommonAncestor(root, 10, 12), "Common Ancestor of 10 & 12 is 6")
+        self.assertEqual(2, lca.lowestCommonAncestor(root, 8, 13), "Common Ancestor of 8 & 13 is 2")
+        self.assertEqual(3, lca.lowestCommonAncestor(root, 6, 9), "Common Ancestor of 6 & 9 is 3")
+
         ## TODO: add more edge case tests as i think of exceptional circuimstances
+
+
+
+
 
 
 if __name__ == '__main__':
